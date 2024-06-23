792429ab9d9c2c6fe53d44424bcd76fa9d22acb8
Stan Wohlwend <n2d4xc@gmail.com>
2024-06-23 18:31:47 +0200

    Test contexts

--- apps/e2e/tests/backend/backend-helpers.ts
+++ apps/e2e/tests/backend/backend-helpers.ts
@@ -1,25 +1,55 @@
 import { filterUndefined } from "@stackframe/stack-shared/dist/utils/objects";
-import { BACKEND_BASE_URL, INTERNAL_PROJECT_ADMIN_KEY, INTERNAL_PROJECT_CLIENT_KEY, INTERNAL_PROJECT_SERVER_KEY, NiceResponse, niceFetch } from "../helpers";
+import { BACKEND_BASE_URL, Context, INTERNAL_PROJECT_ADMIN_KEY, INTERNAL_PROJECT_CLIENT_KEY, INTERNAL_PROJECT_ID, INTERNAL_PROJECT_SERVER_KEY, Mailbox, NiceResponse, createMailbox, niceFetch } from "../helpers";
 import { expect } from "vitest";
+
+type BackendContext = {
+  projectKeys: ProjectKeys,
+  mailbox: Mailbox,
+};
+
+export const backendContext = new Context<BackendContext, Partial<BackendContext>>(
+  () => ({
+    projectKeys: InternalProjectKeys,
+    mailbox: createMailbox(),
+  }),
+  (acc, update) => ({
+    ...acc,
+    ...filterUndefined(update),
+  }),
+);
+
+export type ProjectKeys = "no-project" | {
+  projectId: string,
+  publishableClientKey: string,
+  secretServerKey: string,
+  superSecretAdminKey: string,
+};
+
+export const InternalProjectKeys = {
+  projectId: INTERNAL_PROJECT_ID,
+  publishableClientKey: INTERNAL_PROJECT_CLIENT_KEY,
+  secretServerKey: INTERNAL_PROJECT_SERVER_KEY,
+  superSecretAdminKey: INTERNAL_PROJECT_ADMIN_KEY,
+};
 
 export async function niceBackendFetch(url: string, options?: Omit<RequestInit, "body"> & {
   accessType?: null | "client" | "server" | "admin",
   body?: unknown,
-  internalProject?: boolean,
   headers?: Record<string, string>,
 }): Promise<NiceResponse> {
-  const { body, headers, internalProject, accessType, ...otherOptions } = options ?? {};
+  const { body, headers, accessType, ...otherOptions } = options ?? {};
+  const projectKeys = backendContext.value.projectKeys;
   const res = await niceFetch(new URL(url, BACKEND_BASE_URL), {
     ...otherOptions,
     ...body !== undefined ? { body: JSON.stringify(body) } : {},
     headers: filterUndefined({
       "content-type": body !== undefined ? "application/json" : undefined,
       "x-stack-access-type": accessType ?? undefined,
-      ...internalProject ? {
-        "x-stack-project-id": "internal",
-        "x-stack-publishable-client-key": INTERNAL_PROJECT_CLIENT_KEY,
-        "x-stack-secret-server-key": INTERNAL_PROJECT_SERVER_KEY,
-        "x-stack-super-secret-admin-key": INTERNAL_PROJECT_ADMIN_KEY,
+      ...projectKeys !== "no-project" ? {
+        "x-stack-project-id": projectKeys.projectId,
+        "x-stack-publishable-client-key": projectKeys.publishableClientKey,
+        "x-stack-secret-server-key": projectKeys.secretServerKey,
+        "x-stack-super-secret-admin-key": projectKeys.superSecretAdminKey,
       } : {},
       ...Object.fromEntries(new Headers(headers).entries()),
     }),
@@ -32,4 +62,36 @@ export async function niceBackendFetch(url: string, options?: Omit<RequestInit, 
     });
   }
   return res;
+}
+
+
+type SendSignInCodeResult = {
+  sendSignInCodeEndpointResponse: NiceResponse,
+};
+export async function sendSignInCode(options: {} = {}): Promise<SendSignInCodeResult> {
+  const mailbox = backendContext.value.mailbox;
+  const response = await niceBackendFetch("/api/v1/auth/otp/send-sign-in-code", {
+    method: "POST",
+    accessType: "client",
+    body: {
+      email: mailbox.emailAddress,
+      redirectUrl: "http://localhost:12345",
+    },
+  });
+  expect(response).toMatchInlineSnapshot(`
+    NiceResponse {
+      "status": 200,
+      "body": { "success": true },
+      "headers": Headers {
+        "x-stack-request-id": <stripped header 'x-stack-request-id'>,
+        <some fields may have been hidden>,
+      },
+    }
+  `);
+  const messages = await mailbox.fetchMessages({ subjectOnly: true });
+  const subjects = messages.map((message) => message.subject);
+  expect(subjects).toContain("Sign in to Stack Dashboard");
+  return {
+    sendSignInCodeEndpointResponse: response,
+  };
 }
